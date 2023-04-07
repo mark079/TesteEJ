0eeed8a2e9a8e5ddf44e20261ad4a42ca1ca967f
Mark079 <joaopedromaximo@gmail.com>
2023-04-07 01:24:50 -0300

    translated comment

--- src/common/mask.js
+++ src/common/mask.js
@@ -1,8 +1,8 @@
 export const cpfMask = value => {
     return value
-        .replace(/\D/g, '') // substitui qualquer caracter que nao seja numero por nada
-        .replace(/(\d{3})(\d)/, '$1.$2') // captura 2 grupos de numero o primeiro de 3 e o segundo de 1, apos capturar o primeiro grupo ele adiciona um ponto antes do segundo grupo de numero
+        .replace(/\D/g, '') // replace any non-numeric character with nothing
+        .replace(/(\d{3})(\d)/, '$1.$2') // capture 2 groups of numbers the first of 3 and the second of 1, after capturing the first group add a dot before the second group of numbers
         .replace(/(\d{3})(\d)/, '$1.$2')
         .replace(/(\d{3})(\d{1,2})/, '$1-$2')
-        .replace(/(-\d{2})\d+?$/, '$1') // captura 2 numeros seguidos de um traço e não deixa ser digitado mais nada
+        .replace(/(-\d{2})\d+?$/, '$1') // captures 2 numbers followed by a dash and does not let anything else be typed
 };
